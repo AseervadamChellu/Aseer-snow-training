19840f19af49a8cca99593c3682e9c73d2760dd6
AseervadamChellu <ch.aseervadam@gmail.com>
2024-05-08 20:10:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,8 @@ st.success('Your Smoothie is ordered!', icon="✅")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(),use_contrainer_width=true)
 
 
         

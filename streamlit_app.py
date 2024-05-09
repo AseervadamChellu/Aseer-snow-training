e66bf07d38eef28c0b5f7731288077fa52de17fc
AseervadamChellu <ch.aseervadam@gmail.com>
2024-05-09 11:25:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,12 +24,19 @@ pd_df = my_dataframe.to_pandas()
 #st.stop() 
 ingredients_list =st.multiselect('choose upto 5 ingredients:', my_dataframe , max_selections=5)
 
-st.success('Your Smoothie is ordered!', icon="✅")
+#st.success('Your Smoothie is ordered!', icon="✅")
 
 if ingredients_list:
     ingredients_string = ''
     for fruit_choosen in ingredients_list:
             ingredients_string += fruit_choosen + ' '
+            my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """') """
+            time_to_insert = st.button('Submit Order')
+            if time_to_insert:
+            session.sql(my_insert_stmt).collect()
+            st.success('Your Smoothie is ordered!', icon="✅")
+
             search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_choosen , 'SEARCH_ON'].iloc[0]
             st.write('The search value for ' ,fruit_choosen , ' is ' , search_on , '.') 
             st.subheader(fruit_choosen + ' Nutrition Information')
